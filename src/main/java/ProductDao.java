219f823da3dfa065421cf86efa81554639e7a731
Ingrid-Alice Bløtekjær <you@example.com>
2019-10-08 22:54:28 +0200

    adds new dependency

--- src/main/java/ProductDao.java
+++ src/main/java/ProductDao.java
@@ -1,15 +1,19 @@
 
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 
 public class ProductDao {
-    public void insertProduct(String apples) {
 
+    private List<String> products = new ArrayList<>();
+
+    public void insertProduct(String productName) {
+        products.add(productName);
     }
 
     public List<String> listAll() {
-        return Collections.singletonList("Apples");
+        return products;
     }
 }
--- src/test/java/WebshopTest.java
+++ src/test/java/WebshopTest.java
@@ -1,4 +1,7 @@
 import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WebshopTest {
@@ -6,8 +9,13 @@ public class WebshopTest {
     @Test
     void shouldRetrieveStoredProduct() {
         ProductDao dao = new ProductDao();
-        dao.insertProduct("Apples");
-        assertThat(dao.listAll()).contains("Apples");
+        String productName = pickOne(new String[] {"Apples", "Bananas", "Coconuts", "Dates"});
+        dao.insertProduct(productName);
+        assertThat(dao.listAll()).contains(productName);
+    }
+
+    private String pickOne(String[] strings) {
+        return strings[new Random().nextInt(strings.length)];
     }
 
 }
