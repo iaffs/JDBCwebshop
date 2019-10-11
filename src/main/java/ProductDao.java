6369c499bce604774c597348bebec18e342be130
Ingrid-Alice Bløtekjær <you@example.com>
2019-10-11 21:22:53 +0200

    second green test

--- src/main/java/ProductDao.java
+++ src/main/java/ProductDao.java
@@ -1,12 +1,18 @@
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class ProductDao {
-    public void insertProduct(String apples) {
 
+
+public class ProductDao {
+
+    private List<String> products = new ArrayList<>();
+
+    public void insertProduct(String productName) {
+        products.add(productName);
     }
 
     public List<String> listAll() {
-        return Collections.singletonList("Apples");
+        return products;
     }
 }
