9c5ec404da91b0425c091fd2b97adb058443a42e
Ingrid-Alice Bløtekjær <you@example.com>
2019-10-11 23:41:56 +0200

    adds database connection

--- src/main/java/ProductDao.java
+++ src/main/java/ProductDao.java
@@ -1,5 +1,8 @@
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 
@@ -7,9 +10,23 @@ import java.util.List;
 public class ProductDao {
 
     private List<String> products = new ArrayList<>();
+    private DataSource dataSource;
+
+    public ProductDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     public void insertProduct(String productName) {
         products.add(productName);
+
+        try (Connection conn = dataSource.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement("insert into products (name) values (?)");
+            statement.setString(1, productName);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public List<String> listAll() {
